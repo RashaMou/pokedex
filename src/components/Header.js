cf58c84a6cc7ef079f795da1d77041e8cd31023d
Rasha Moumneh <rasha@fastmail.com>
2020-07-10 18:35:17 -0400

    Add styling - create header - import fonts

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <section class='hero is-bold is-warning'>
+      <div class='hero-body'>
+        <div class='container'>
+          <h1 className='header-title title is-1'>Pokedex</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
