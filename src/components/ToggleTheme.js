43ba963bba90a68d02dbbad41cf82ac8a0f03adf
Rasha Moumneh <rasha@fastmail.com>
2020-07-11 16:53:36 -0400

    Change dark mode toggle to MUI icons

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,37 +1,20 @@
 import React, { useContext } from 'react';
-import WbSunnyIcon from '@material-ui/icons/WbSunny';
-import Brightness3Icon from '@material-ui/icons/Brightness3';
+import IconButton from '@material-ui/core/IconButton';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
 import { ThemeContext } from '../contexts/ThemeContext';
-import Switch from '@material-ui/core/Switch';
-import useLocalStorage from '../hooks/useLocalStorage';
 
 const ToggleTheme = () => {
   const { toggleTheme, isDarkTheme } = useContext(ThemeContext);
-  const [icon, setIcon] = useLocalStorage(false);
-
-  const iconChange = () => {
-    setIcon(!icon);
-  };
-
-  const handleSwitch = () => {
-    toggleTheme();
-    iconChange();
-  };
-
   return (
     <div className='toggle__box'>
-      <span>
-        {icon ? (
-          <Brightness3Icon className='moon-icon' alt='moon icon' />
+      <IconButton onClick={() => toggleTheme()}>
+        {isDarkTheme ? (
+          <Brightness4Icon className='moon-icon' alt='moon icon' />
         ) : (
-          <WbSunnyIcon className='sun-icon' alt='sun icon' />
+          <Brightness7Icon className='sun-icon' alt='sun icon' />
         )}
-      </span>
-      <Switch
-        checked={isDarkTheme}
-        onChange={() => handleSwitch()}
-        color='default'
-      />
+      </IconButton>
     </div>
   );
 };
