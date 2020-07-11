06664a6cc06a31de9f90ebb38c4dd515a28940c5
Rasha Moumneh <rasha@fastmail.com>
2020-07-10 22:25:02 -0400

    Add animation to card upon first load

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,8 +1,8 @@
-// utility function that returns a random number between 1 and 890 (the total number of pokemon images in https://pokeres.bastionbot.org)
+// utility function that returns a random number between 1 and 807 (the total number of pokemon images in https://pokeres.bastionbot.org)
 // I'm using https://pokeres.bastionbot.org for images because the sprites in the pokeapi are very lo-res
 
 const getRandomNumber = () => {
-  return Math.floor(Math.random() * Math.floor(890));
+  return Math.floor(Math.random() * Math.floor(807));
 };
 
 export default getRandomNumber;
